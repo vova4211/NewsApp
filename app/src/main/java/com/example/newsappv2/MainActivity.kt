25af3b24a0a749085ac9e66d60025b7878fd8b89
Volodymyr Viienko <vovavienko@gmail.com>
2026-03-23 18:22:42 +0200

    fix bugs

--- app/src/main/java/com/example/newsappv2/MainActivity.kt
+++ app/src/main/java/com/example/newsappv2/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.newsappv2
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 import com.example.newsappv2.ui.NewsApp
+import com.example.newsappv2.ui.theme.NewsAppV2Theme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            NewsApp()
+            NewsAppV2Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    NewsApp()
+                }
+            }
         }
     }
 }
